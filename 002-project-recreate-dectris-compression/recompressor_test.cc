2dca502720f9abf07ce2834111c76ecc6bd4c890
Graeme Winter <graeme.winter@gmail.com>
2024-10-18 13:22:36 +0100

    Start of stubs for recompression exercise

---
+++ 002-project-recreate-dectris-compression/recompressor_test.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include <hdf5.h>
+
+extern "C" {
+#include "bitshuffle.h"
+#include "lz4.h"
+}
+
+int main(int argc, char **argv) {
+
+  if (argc == 1) {
+    std::cout << argv[0] << " data_00001.h5" << std::endl;
+    return 1;
+  }
+
+  hid_t file = H5Fopen(argv[1], H5F_ACC_RDONLY, H5P_DEFAULT);
+
+  if (file < 0) {
+    std::cout << argv[1] << " is not a valid HDF5 file" << std::endl;
+    return 1;
+  }
+
+  hid_t data = H5Dopen(file, "/data", H5P_DEFAULT);
+  if (data < 0) {
+    H5Fclose(file);
+    std::cout << argv[1] << "/data is not a valid HDF5 dataset" << std::endl;
+    return 1;
+  }
+
+  hid_t dspace = H5Dget_space(data);
+  const int ndims = H5Sget_simple_extent_ndims(dspace);
+
+  if (ndims != 3) {
+    std::cout << "ndims != 3" << std::endl;
+    H5Dclose(data);
+    H5Fclose(file);
+    return 1;
+  }
+
+  hsize_t dims[ndims];
+  H5Sget_simple_extent_dims(dspace, dims, NULL);
+
+  int NZ = dims[0];
+  int NY = dims[1];
+  int NX = dims[2];
+
+  hsize_t max_chunk = 0;
+
+  for (int nz = 0; nz < NZ; nz++) {
+    hsize_t chunk_size = 0;
+    hsize_t off[ndims];
+    off[0] = nz;
+    off[1] = 0;
+    off[2] = 0;
+    H5Dget_chunk_storage_size(data, off, &chunk_size);
+    max_chunk = std::max(max_chunk, chunk_size);
+    std::cout << nz << " " << chunk_size << std::endl;
+  }
+
+  return 0;
+}
