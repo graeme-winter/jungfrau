70975776fce6b0d7f5965fe0e0127e6ad7d66195
Graeme Winter <graeme.winter@gmail.com>
2024-12-09 09:32:08 +0000

    Tinkering timing

--- 007-zeromq/stream_capture.py
+++ 007-zeromq/stream_capture.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 
 import zmq
 
@@ -12,13 +13,27 @@ socket.setsockopt(zmq.SUBSCRIBE, b"")
 socket.setsockopt(zmq.RCVHWM, 50000)
 socket.connect(f"tcp://{host}:{port}")
 
+frames = []
+
+t0 = None
+
 while True:
     messages = socket.recv_multipart()
     header = json.loads(messages[0])
     if header["bitmode"] == 0:
         break
-    n = header["frameNumber"]
-    if not n % 100:
+    n = header["frameIndex"]
+
+    if n == 0:
+        t0 = time.time()
+
+    if not n % 1000:
         print(n)
-        print(header)
-        print(len(messages[1]))
+    frames.append(n)
+
+t1 = time.time()
+
+print(f"Read {n+1} frames in {t1 - t0:.2f}s")
+
+assert len(frames) == len(set(frames))
+
