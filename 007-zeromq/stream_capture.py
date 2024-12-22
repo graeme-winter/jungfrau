276cafc17db571d006c63fa52c65db0d0e465c0c
Graeme Winter <graeme.winter@gmail.com>
2024-12-22 06:13:54 +0000

    Timestamp

--- 007-zeromq/stream_capture.py
+++ 007-zeromq/stream_capture.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 
 import zmq
 
@@ -12,13 +13,15 @@ socket.setsockopt(zmq.SUBSCRIBE, b"")
 socket.setsockopt(zmq.RCVHWM, 50000)
 socket.connect(f"tcp://{host}:{port}")
 
+t0 = -1
+
 while True:
     messages = socket.recv_multipart()
+    if t0 < 0:
+        t0 = time.time()
     header = json.loads(messages[0])
     if header["bitmode"] == 0:
         break
-    n = header["frameNumber"]
-    if not n % 100:
-        print(n)
-        print(header)
-        print(len(messages[1]))
+    n = header["frameIndex"] + 1
+    if not n % 1000:
+        print(n, time.time() - t0)
