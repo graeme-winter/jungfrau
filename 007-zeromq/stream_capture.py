b4cd990300d569d76ae5af6e5c914a20667ae8f1
Graeme Winter <graeme.winter@gmail.com>
2024-12-24 06:01:11 +0000

    Add time & buffer

--- 007-zeromq/stream_capture.py
+++ 007-zeromq/stream_capture.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 
 import zmq
 
@@ -8,17 +9,29 @@ port = int(sys.argv[1])
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
+
 socket.setsockopt(zmq.SUBSCRIBE, b"")
+socket.setsockopt(zmq.RCVBUF, 128 * 1024 * 1024)
 socket.setsockopt(zmq.RCVHWM, 50000)
+
 socket.connect(f"tcp://{host}:{port}")
+
+found = []
+
+t0 = 0
 
 while True:
     messages = socket.recv_multipart()
+    if not t0:
+        t0 = time.time()
     header = json.loads(messages[0])
     if header["bitmode"] == 0:
         break
-    n = header["frameNumber"]
-    if not n % 100:
-        print(n)
-        print(header)
-        print(len(messages[1]))
+    n = int(header["frameNumber"])
+    found.append(n)
+
+t1 = time.time()
+
+assert len(found) == max(found) + 1
+
+print(f"{t1 - t0:.2f}s")
